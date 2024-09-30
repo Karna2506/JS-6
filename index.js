4cbb3eaef203a06b480fc8db0449239d2a04263b
Mallikarjuna_Reddy_25 <137518088+Karna2506@users.noreply.github.com>
2024-09-30 19:31:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,91 @@
+
+                            // Task-1
+
+function showmessage(){
+    console.log('"Hello, Welocome To The Website!.."')
+}
+showmessage();
+
+                            // Task-2
+
+function countToTen(){
+    for ( var i= 1; i <= 10; i++){
+        console.log(i)
+    }
+  
+}
+countToTen();
+
+                            // Task-3
+
+function multiplicationTable(){
+    for(var i = 1; i <=10; i++){
+        console.log(2 + 'x' + i + '=' ,(2*i)) 
+    }
+}
+multiplicationTable();
+
+
+                            // Task-4
+
+function showOddNumbers(){
+    for(var i = 1; i <= 10; i++){
+        if(i%2 ==1){
+            console.log(i)
+        }
+    }
+}
+showOddNumbers();
+
+
+                           // Task-5
+
+function countDown(){
+    for( var i = 5; i >= 1; i--){
+        console.log(i);
+    }
+}
+countDown();
+
+                           // Task-6                        
+
+function greetUser(){
+    console.log('"Good day, user!"')
+}
+greetUser();
+
+                        // Task-7
+
+function showEvenNumbers(){
+    for(var i = 1; i <= 10; i++){
+        if(i%2 ==0){
+            console.log(i)
+        }
+    }
+}
+showEvenNumbers();
+
+                         // Task-8
+
+function farewellMessage(){
+    console.log('"Thank You for visiting, goodbye!.."')
+}
+farewellMessage();
+
+                          // Task-9
+
+function ShowSquares(){
+    for(var i=1;i<=5;i++){
+        console.log(i*i)
+    }
+}
+ShowSquares();
+
+                          // Task-10
+
+function repeatWelcome(){
+    for (var i=1;i <= 3;i++){
+        console.log('"Welcome!..')
+    }
+}
+repeatWelcome();
